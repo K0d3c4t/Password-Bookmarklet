00c2b5f24ce51daf5f1328cb81e872d04ee0c5c5
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-07 12:31:31 -0500

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,5 +1,6 @@
 var password = prompt("Please enter password");  
-if (password != null){ if (password == "test") {
+{ if (password == "test") {
 alert("Access Granted" );
-window.location.href =  "https://docs.google.com/document/d/1-FO6_JF_3XAHcg9VxR0SGJN6dh0ADatvXK3ONkcIXKo/edit";}} else{   
-alert("Access Denied" );}
+window.location.href =  "google.com";} else{ 
+if (password != null)  
+alert("Access Denied" );}};
