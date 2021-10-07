2128d0562ee298521d8f70fbad88b9a70d7b0d66
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-07 11:52:10 -0500

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,5 @@
+var password = prompt("Please enter password");  
+if (password != null){ if (password == "test") {
+alert("Access Granted" );
+window.location.href =  "https://docs.google.com/document/d/1-FO6_JF_3XAHcg9VxR0SGJN6dh0ADatvXK3ONkcIXKo/edit";}} else{   
+alert("Access Denied" );}
