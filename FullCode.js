a19590b187e0f0380d8d59cd49a577abae23cc85
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-12 07:19:55 -0500

    Update and rename FullCode.js to MainCode.js

--- FullCode.js
+++
@@ -1,6 +0,0 @@
-var password = prompt("Please enter password");  
-{ if (password == "test") {
-alert("Access Granted" );
-window.location.href =  "google.com";} else{ 
-if (password != null)  
-alert("Access Denied" );}};
---
+++ MainCode.js
@@ -0,0 +1,17 @@
+var password = prompt("Please enter password");  
+{ if (password == "test") {
+alert("Access Granted" );
+window.location.href =  "google.com";} else{ 
+if (password != null)  
+alert("Access Denied" );}};
+/*
+function passwordField(){theWindow = window.open('','newwin','width=100,height=100');
+if (!theWindow.opener) theWindow.opener=self;Text = '<form ';Text +='onSubmit="opener.location=this.password.value + \'.html\'; self.close()">';
+Text += '<input type="password" name="password">';
+Text += '<\/form>';    
+theWindow.document.open();    
+theWindow.document.write(Text);
+theWindow.document.close();}
+alert(opener.location);
+passwordField();
+*/
