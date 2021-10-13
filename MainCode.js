321c2c7ea010d6393f8ef1e2356763310c91e4ff
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-13 14:17:10 -0500

    Update MainCode.js

--- MainCode.js
+++ MainCode.js
@@ -6,7 +6,11 @@ if (password != null)
 alert("Access Denied" );}};
 /*
 function passwordField(){theWindow = window.open('','newwin','width=100,height=100');
-if (!theWindow.opener) theWindow.opener=self;Text = '<form ';Text +='onSubmit="opener.location=this.password.value + \'.html\'; self.close();alert(this.password.value);">';
+if (!theWindow.opener) theWindow.opener=self;Text = '<form ';Text +='onSubmit="opener.location=this.password.value + \'.html\'; self.close();if (this.password.value==test) {
+alert(1);
+} else {
+alert(2);
+};">';
 Text += '<input type="password" name="password">';
 Text += '<\/form>';    
 theWindow.document.open();    
